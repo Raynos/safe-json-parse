417f469d465f6945a839caad286ca87657d2d2ad
Raynos <raynos2@gmail.com>
2013-11-26 21:26:21 -0800

    bugfix

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 module.exports = SafeParse
 
-function SafeParse(obj, reviver) {
-    return function (callback) {
-        var json
-
-        try {
-            json = JSON.parse(obj, reviver)
-        } catch (err) {
-            return callback(err)
-        }
-
-        callback(null, json)
+function SafeParse(obj, reviver, callback) {
+    if (arguments.length === 2) {
+        callback = reviver
+        reviver = null
     }
+
+    var json
+
+    try {
+        json = JSON.parse(obj, reviver)
+    } catch (err) {
+        return callback(err)
+    }
+
+    callback(null, json)
 }
